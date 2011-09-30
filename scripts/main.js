1bdc0b94c757375d049714f0f1732d0a07769597
Chris Lee <chris@chrsl.net>
2011-09-30 19:22:43 -0400

    Autocomplete + item removal

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,40 @@ function addEntry() {
     console.log(data);
   });
 
+  addEntryEl('1pm', location);
+}
+
+function addEntryEl(time, location) {
   View('entry')
-    .time('1pm')
+    .time(time)
     .location(location)
+    .remove(function() { this.el.slideUp(); })
     .hide()
     .appendTo('.entries')
     .el.slideDown();
+}
+
+function autocomplete() {
+  var selected;
+
+  $('.add_entry_submit').click(function() {
+    if (selected) addEntryEl('1pm', selected)
+  });
+
+  $('.add_entry_input').smartAutoComplete({
+      source: ['Taj Mahal', 'Great Wall of China', 'Pyramid of Giza', 'Carnegie Mellon University']
+    , resultsContainer: '.add_entry_results'
+  }).bind({
+      itemFocus: function(item) {
+        selected = item.smartAutocompleteData.item && item.smartAutocompleteData.item.innerText;
+      }
+    , itemSelect: function(item) {
+        selected = item.smartAutocompleteData.item && item.smartAutocompleteData.item.innerText;
+        if (selected) addEntryEl('1pm', selected);
+      }
+    , showResults: function() { $('.add_entry_results').show(); }
+    , hideResults: function() { $('.add_entry_results').hide(); }
+  });
 }
 
 $(function() {
@@ -31,11 +59,15 @@ $(function() {
     .location('Pyramid of Giza')
     .appendTo('.entries');
 
+  autocomplete();
 
-  $('.add_entry_submit').click(addEntry);
-
-  $('.add_entry_input').bind('keypress', function(e) {
-    var code = (e.keyCode ? e.keyCode : e.which);
-    if (code == 13) addEntry();
+  /*
+  var req = new FlickrRequest(-79.943047,40.443028,'carnegie');
+  req.doQuery(function(images) {
+    for (var i = 0, len = images.length; i < len; i++) {
+      var image = images[i];
+      $('<img>').attr('src', image).appendTo('.images');
+    }
   });
+  */
 });
