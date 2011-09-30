fce71c44913cd58c2071d19f82278d293d0844e9
Chris Lee <chris@chrsl.net>
2011-09-30 14:47:00 -0400

    'Add stop' functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,25 @@
 $(function() {
+  // dummy data
+  View('entry')
+    .time('12:30pm')
+    .location('Taj Mahal')
+    .appendTo('.entries');
+
+  View('entry')
+    .time('2pm')
+    .location('Great Wall of China')
+    .appendTo('.entries');
+
+  View('entry')
+    .time('3pm')
+    .location('Pyramid of Giza')
+    .appendTo('.entries');
+
+
   $('.add_entry').click(function() {
+    View('entry')
+      .time('1pm')
+      .location('Carnegie Mellon University')
+      .appendTo('.entries');
   });
 });
