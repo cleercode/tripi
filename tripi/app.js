21ac75111d02652c30a0fb161ba5a82e4cb2a2ca
Chris Lee <chris@chrsl.net>
2011-10-01 00:11:44 -0400

    Jade to EJS

--- tripi/app.js
+++ tripi/app.js
@@ -12,7 +12,7 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -31,18 +31,19 @@ app.configure('production', function(){
 
 app.get('/', function(req, res) {
   res.render('index', {
-    title: 'Express'
+      title: 'Express'
+    , layout: false
   });
 });
 
 app.get('/search', function(req, res) {
   var query = req.param('query')
-  , url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + encodeURIComponent(query) +
-          '&types=establishment&location=40,-80&radius=500&sensor=true&key=AIzaSyAyhFF7BBmfMhRZElunBN1rsfh-UEfohEM';
+    , url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + encodeURIComponent(query) +
+            '&types=establishment&location=40,-80&radius=500&sensor=true&key=AIzaSyAyhFF7BBmfMhRZElunBN1rsfh-UEfohEM';
   request.get(url, function(error, response, body) {
     res.send(body);
   });
-})
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
